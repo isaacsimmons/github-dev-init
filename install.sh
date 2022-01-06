f3b2c3bfd563dcc33911636d93d88b173367c9de
Isaac Simmons <isaac.simmons@gmail.com>
2022-01-06 18:31:12 -0500

    Prelim support for multiple github hosts

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CONFIG_DIR="${CONFIG_DIR:~/.dotfiles}"
 # rename to dotfiles dir?
+CONFIG_DIR="${CONFIG_DIR:-$HOME/.dotfiles}"
+CONFIG_FILE="${CONFIG_DIR}/github.env"
+echo "Using config in $CONFIG_DIR"
 
 # Define some helper functions
 exiterr() {
@@ -12,10 +14,38 @@ exiterr() {
   exit 1
 }
 
-requireenv() {
-  local VARNAME="${1}"
-  >&2 echo "ERROR: ${1}"
-  exit 1
+require-env() {
+  local var_name="${1}"
+  [ -z "${!var_name:-}" ] && exiterr "${CONFIG_FILE} missing required environtment variable: ${var_name}"
+  return 0
+}
+
+github-env-name() {
+  local host="${1}"
+  local suffix="${2}"
+  echo "$( echo "$host" | tr [:lower:] [:upper:] | tr . _ )_$suffix"
+}
+
+check-github-auth() {
+  local host="${1}"
+
+#  echo $AUTH_TOKEN_NAME
+#  require-env $AUTH_TOKEN_NAME
+#  echo ${!AUTH_TOKEN_NAME}
+
+  set +e
+  gh auth status --hostname $host &> /dev/null
+  GH_AUTH_STATUS="$?"
+  set -e
+  if [ "${GH_AUTH_STATUS}" -ne "0" ]; then
+    AUTH_TOKEN_NAME="$( github-env-name $host AUTH_TOKEN )"
+    require-env $AUTH_TOKEN_NAME
+    echo "${!AUTH_TOKEN_NAME}" | gh auth login --hostname $host --with-token
+  else
+    echo "GH CLI authenticated"
+  fi
+
+  # Push your SSH key if not already present
 }
 
 [ "${EUID}" -eq 0 ] && exiterr "Don't run this as root"
@@ -61,11 +91,10 @@ fi
 
 # Setup config dir and copy config template if missing
 [ -d "${CONFIG_DIR}" ] || mkdir -p "${CONFIG_DIR}"
-[ -f "${CONFIG_DIR}/.env" ] || cp "${SCRIPT_DIR}/.env.template" "${CONFIG_DIR}/.env"
+[ -f "${CONFIG_FILE}" ] || cp "${SCRIPT_DIR}/github.env.template" "${CONFIG_FILE}"
 
 # Load in environment vars, apply defaults as needed
-source "${CONFIG_DIR}/.env"
-REPO_ROOT_DIR="${REPO_ROOT_DIR:-/Volumes/dev}"
+source "${CONFIG_FILE}"
 
 # If there's a .dotfiles-links file in your config dir already, ensure that all of those links have been created
 # Note: this is pretty early in the process, so if you have your sshkeys or git config in there, it'll be linked
@@ -75,18 +104,19 @@ if [ -f "${CONFIG_DIR}/.dotfile-links" ]; then
 fi
 
 # Setup repo root directory
+REPO_ROOT_DIR="${REPO_ROOT_DIR:-$HOME/code}"
 [ -d "${REPO_ROOT_DIR}" ] || mkdir -p "${REPO_ROOT_DIR}"
 
 # Make sure we have SSH keys setup as expected
 # TODO: actually, I want them to be symlinks into my config directory???
-if [ ! -f ~/.ssh/id_rsa ]; then
+if [ ! -f ${HOME}/.ssh/id_rsa ]; then
   # create one if missing
-  exiterr "No SSH key found"  
+  exiterr "No SSH key found"
 fi
 
 # Setup 
-[ -z "${GIT_DISPLAY_NAME}" ] || echoerr "GIT_DISPLAY_NAME undefined"
-[ -z "${GIT_EMAIL}" ] || echoerr "GIT_EMAIL undefined"
+require-env GIT_DISPLAY_NAME
+require-env GIT_EMAIL
 if [ "$(git config --global --get user.name)" != "${GIT_DISPLAY_NAME}" ]; then
   echo "Setting git config user.name"
   git config --global user.name "${GIT_DISPLAY_NAME}"
@@ -102,20 +132,12 @@ if [ "$(gh config get git_protocol)" != "ssh" ]; then
   gh config set git_protocol ssh
 fi
 
-# Make sure the GH CLI is authenticated
-# TODO: additional github hostnames
-set +e
-gh auth status --hostname github.com &> /dev/null
-GH_AUTH_STATUS="$?"
-set -e
-if [ "${GH_AUTH_STATUS}" -ne "0" ]; then
-  [ -z "${GITHUB_AUTH_TOKEN}" ] || echoerr "GITHUB_AUTH_TOKEN undefined"
-  echo "${GITHUB_AUTH_TOKEN}" | gh auth login --hostname github.com --with-token
-else
-  echo "GH CLI authenticated"
-fi
+# Make sure the GH CLI is authenticated with all defined github hosts
+GITHUB_HOSTS="${GITHUB_HOSTS:-GITHUB}"
+for GITHUB_HOST in ${GITHUB_HOSTS//,/$IFS}; do
+  check-github-auth $GITHUB_HOST
+done
 
-# Push your SSH key to github(s) if not already present
 
 [ -f "${CONFIG_DIR}/repo-list.txt" ] || cp "${SCRIPT_DIR}/repo-list.txt.template" "${CONFIG_DIR}/repo-list.txt"
 
